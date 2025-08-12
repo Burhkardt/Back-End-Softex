63b92376f708b0e7f90747ee115cf8de3af12358
Juliandre César <77554865+Burhkardt@users.noreply.github.com>
2025-08-12 18:45:40 +0000

    Realização dos exercícios

---
+++ Aula_3/Aula3.py
@@ -0,0 +1,20 @@
+print("Exercício 1:")
+print("Olá, Mundo!")
+print("\n")
+print("Exercício 2:")
+print("O rato roeu a roupa do rei de Roma.")
+print("Três pratos de trigo para três tigres tristes.")
+print("Bottom text.")
+print("\n")
+print("Exercício 3:")
+print("Nome:    João\n"\
+      "Idade:   20 Anos\n"\
+      "Curso:   Python Básico\n")
+print("\n")
+print("Exercício 4:")
+nome = "Juliandre"
+idade = 23
+curso = "Python"
+print(f"Nome:   {nome}\n"\
+      f"Idade:  {idade}\n"\
+      f"Curso:  {curso}")
